275f481e6a1b7c70f48690ed80f6ab97daa47ec9
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-09-08 01:10:17 +0300

    Add methods to read file and get lines

--- lib/file_actions.rb
+++ lib/file_actions.rb
@@ -1,11 +1,23 @@
 require 'find'
 
-module File_actions
+module FileActions
   def self.get_file_paths
     files = []
     Find.find('./') do |path|
       files << path if File.extname(path) == '.json'
     end
     files
+  end
+
+  def open_file(path)
+    file = File.open(path, 'r')
+  end
+
+  def read_lines(file)
+    file.readlines
+  end
+
+  def count_lines(file)
+    file.readlines.size
   end
 end
--- spec/file_actions_spec.rb
+++ spec/file_actions_spec.rb
@@ -1,9 +1,30 @@
 require_relative '../lib/file_actions'
 
-describe File_actions do
+describe FileActions do
   describe '#get_file_paths' do
     it ('returns an array of all paths to .json files in all project directories') do
-      expect(File_actions.get_file_paths).to eql(["./test_files/test1.json", "./test_files/test2.json"])
+      expect(FileActions.get_file_paths).to eql(["./test_files/test1.json", "./test_files/test2.json"])
+    end
+  end
+
+  class TestFile; include FileActions end
+  let(:test_file) { TestFile.new }
+
+  describe '#open_file' do
+    it ('opens the file at provided path') do
+      expect(test_file.open_file('./test_files/test1.json').readchar).to eql('{')
+    end
+  end
+
+  describe '#read_file_lines' do
+    it ('returns an array of the lines in the given file') do
+      expect(test_file.read_lines(File.open('./test_files/test1.json', 'r'))).to be_instance_of(Array)
+    end
+  end
+
+  describe '#count_lines' do
+    it ('returns the number of lines in the given file') do
+      expect(test_file.count_lines(File.open('./test_files/test1.json', 'r'))).to be_instance_of(Integer)
     end
   end
 end
